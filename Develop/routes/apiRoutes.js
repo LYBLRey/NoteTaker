0c36e05b7a534904d6db57f78d86212fce085890
Sareybodd Chhorn <sareyboddc@gmail.com>
2021-09-09 01:53:30 -0700

    apiRoutes and htmlRoutes js code added

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,3 +1,20 @@
 const fs = require("fs")
 const router = require("express").Router()
 const db = require("../db/db.json")
+
+router.get("/notes", (req, res) => res.json(db))
+
+router.post("/notes", (req, res) => {
+  const id = Math.floor(Math.random() * 10000)
+  const title = req.body.title
+  const text = req.body.text
+  const newNotes = {
+    id: id,
+    title: title,
+    text: text,
+  }
+  db.push(newNotes)
+  fs.writeFileSync("../db/db.json", JSON.stringify(db))
+  res.json(db)
+})
+module.exports = router
